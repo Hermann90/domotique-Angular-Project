ba9418e8bde569ec3b792c2c502cac8ca3d84adb
CHEFOUETMEKA Gael Hermann .B <hermannchefouet@gmail.com>
2021-01-24 12:30:23 +0100

    manage dynamic object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponentComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule,
